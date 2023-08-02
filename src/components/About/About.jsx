ace8e623448a50da79938a3499af9dfbf3ba9129
Veronika Vokounová <ver.cik@seznam.cz>
2023-08-03 00:12:35 +0200

    aktualizace

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,60 +1,58 @@
-import React, {useEffect} from 'react'
-import './about.css'
-import {FiAward} from 'react-icons/fi'
-import Aos from 'aos';
-import 'aos/dist/aos.css'
+import React, { useEffect } from "react";
+import "./about.css";
+import { FiAward } from "react-icons/fi";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const About = () => {
-
   useEffect(() => {
-    Aos.init({duration: 2000});
+    Aos.init({ duration: 2000 });
   }, []);
 
   return (
-    
-    <section id='about' >
-      <div className='grids' data-aos="fade-up">
-      <h5>Get To Know</h5>
-      <h2>About Me</h2>
-      <div className="container about__container">
-        <div className="about__me">
-          <div className="about__me-img">
-          <div className='about__img'></div>
-          </div>
-            
-        </div>
-
-        <div className="about__content">
-          <div className="about__cards">
-            <article className='about__card'>
-              <FiAward className='about__icon'/>
-              <h5>Python</h5>
-              <small>Udemy, Czechitas</small>
-            </article>
-
-            <article className='about__card'>
-              <FiAward className='about__icon'/>
-              <h5>Javascript, React</h5>
-              <small>Udemy, Czechitas</small>
-            </article>
-
-            <article className='about__card'>
-              <FiAward className='about__icon'/>
-              <h5>HTML & CSS</h5>
-              <small>Udemy, Czechitas</small>
-            </article>
-
+    <section id="about">
+      <div className="grids" data-aos="fade-up">
+        <h5>Get To Know</h5>
+        <h2>About Me</h2>
+        <div className="container about__container">
+          <div className="about__me">
+            <div className="about__me-img">
+              <div className="about__img"></div>
+            </div>
           </div>
 
-          
-          <p>I enjoy exploring and bringing the technical and visual aspects of digital products to life. I'm studying Javascript, React, Python and also recently launched a course that prepares me to become a Software Tester.</p>
+          <div className="about__content">
+            <div className="about__cards">
+              <article className="about__card">
+                <FiAward className="about__icon" />
+                <h5>Python</h5>
+                <small>Udemy, Czechitas</small>
+              </article>
 
+              <article className="about__card">
+                <FiAward className="about__icon" />
+                <h5>Javascript, React</h5>
+                <small>Udemy, Czechitas</small>
+              </article>
+
+              <article className="about__card">
+                <FiAward className="about__icon" />
+                <h5>HTML & CSS</h5>
+                <small>Udemy, Czechitas</small>
+              </article>
+            </div>
+
+            <p>
+              I enjoy exploring and bringing the technical and visual aspects of
+              digital products to life. I'm improving my skills of React, Python
+              and also recently launched a course that prepares me to become a
+              Full Stack Developer.
+            </p>
+          </div>
         </div>
-      </div>
       </div>
     </section>
+  );
+};
 
-  )
-}
-
-export default About
+export default About;
--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,77 +1,64 @@
-import React from 'react'
-import './experience.css'
-import {AiFillHtml5} from 'react-icons/ai'
-import {TbBrandCss3} from 'react-icons/tb'
-import {FaSass} from 'react-icons/fa'
-import {TbBrandBootstrap} from 'react-icons/tb'
-import {SiTailwindcss} from 'react-icons/si'
-import {TbBrandJavascript} from 'react-icons/tb'
-import {FaReact} from 'react-icons/fa'
-
+import React from "react";
+import "./experience.css";
+import { AiFillHtml5 } from "react-icons/ai";
+import { TbBrandCss3 } from "react-icons/tb";
+import { FaSass } from "react-icons/fa";
+import { TbBrandBootstrap } from "react-icons/tb";
+import { SiTailwindcss } from "react-icons/si";
+import { TbBrandJavascript } from "react-icons/tb";
+import { FaReact } from "react-icons/fa";
 
 const Experience = () => {
-
   return (
-    
-    <section id='experience'>
-      <div className='grids' data-aos="fade-up">
+    <section id="experience">
+      <div className="grids" data-aos="fade-up">
         <h5>What Skills I Have</h5>
         <h2>My Experience</h2>
 
-        <div className='container experience__container'>
+        <div className="container experience__container">
           <div className="experience__frontend">
             <h3>Frontend Development</h3>
             <div className="experience__content">
-              <article className='experience__details'>
-                <AiFillHtml5 className='experience__details-icon'/>
+              <article className="experience__details">
+                <AiFillHtml5 className="experience__details-icon" />
                 <h4>HTML</h4>
               </article>
 
-              <article className='experience__details'>
-                <TbBrandCss3 className='experience__details-icon'/>
+              <article className="experience__details">
+                <TbBrandCss3 className="experience__details-icon" />
                 <h4>CSS</h4>
               </article>
 
-              <article className='experience__details'>
-                <FaSass className='experience__details-icon'/>
+              <article className="experience__details">
+                <FaSass className="experience__details-icon" />
                 <h4>Sass</h4>
               </article>
 
-              
-
-              <article className='experience__details'>
-                <TbBrandBootstrap className='experience__details-icon'/>
+              <article className="experience__details">
+                <TbBrandBootstrap className="experience__details-icon" />
                 <h4>Bootstrap</h4>
-                
               </article>
 
-              <article className='experience__details'>
-                <SiTailwindcss className='experience__details-icon'/>
-                <h4>Tailwind</h4> 
+              <article className="experience__details">
+                <SiTailwindcss className="experience__details-icon" />
+                <h4>Tailwind</h4>
               </article>
 
-              <article className='experience__details'>
-                <TbBrandJavascript className='experience__details-icon'/>
+              <article className="experience__details">
+                <TbBrandJavascript className="experience__details-icon" />
                 <h4>Javascript</h4>
-                
               </article>
 
-              <article className='experience__details'>
-                <FaReact className='experience__details-icon'/>
+              <article className="experience__details">
+                <FaReact className="experience__details-icon" />
                 <h4>React</h4>
-                
               </article>
-
-              
-
-
             </div>
           </div>
-
         </div>
-       </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
